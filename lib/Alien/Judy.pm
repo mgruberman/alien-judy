a8d908609357c6e0139e0ca43ec7235eabad58e7
Joshua ben Jore <jjore@cpan.org>
2008-12-20 12:27:18 -0800

    Adding Build to MANIFEST.SKIP Removed two extra copy of Judy from the distro Minor pod fixes Clarified that Judy is licenced separately from Alien::Judy

--- lib/Alien/Judy.pm
+++ lib/Alien/Judy.pm
@@ -1,6 +1,6 @@
 package Alien::Judy;
 
-use version; our $VERSION = '0.01';
+use version; our $VERSION = '0.02';
 
 1;
 
@@ -8,13 +8,12 @@ __END__
 
 =head1 NAME
 
-Alien::Judy - A wrapper for installing the libJudy C library
+Alien::Judy - A wrapper for installing the Judy library
 
 =head1 DESCRIPTION
 
-Alien::Judy is a wrapper to ease installing the libJudy C library from
-L<http://judy.sourceforge.net>. L<Judy::HS> depends on this to ensure
-that libJudy is available for installation.
+Alien::Judy is a wrapper to ease installing the Judy library from
+L<http://judy.sourceforge.net>.
 
 =head1 AUTHOR
 
@@ -22,9 +21,11 @@ Joshua ben Jore, C<< <jjore at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-alien-judy at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Alien-Judy>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests to C<bug-alien-judy at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Alien-Judy>.  I will
+be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
 
 
 
@@ -35,10 +36,9 @@ You can find documentation for this module with the perldoc command.
 
     perldoc Alien::Judy
 
-
 You can also look for information at:
 
-=over 4
+=over
 
 =item * RT: CPAN's request tracker
 
@@ -59,20 +59,23 @@ L<http://search.cpan.org/dist/Alien-Judy/>
 =back
 
 
+
 =head1 ACKNOWLEDGEMENTS
 
+I stole the packaging style from Michael Schwern's L<Alien::SVN>.
 
-=head1 COPYRIGHT & LICENSE
+
+=head1 Alien::Judy COPYRIGHT & LICENSE
 
 Copyright 2008 Joshua ben Jore, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-The included library Judy-1.0.4 is not covered by this license. Please
-consult src/Judy-1.0.4/README for notes about its copyright and
-licensing.
 
-=cut
 
-1; # End of Alien::Judy
+=head1 Judy COPYRIGHT & LICENSE
+
+The included library Judy-1.0.4 is covered by separate copyright and
+license. Please consult the library's source code (L<src/Judy-1.0.4/README>)for notes about its
+copyright and licensing.
