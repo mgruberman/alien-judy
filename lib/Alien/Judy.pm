78d0f5898b3facb6a45ebc58628e30744591b36a
Josh ben Jore <jjore@cpan.org>
2010-05-09 20:54:04 -0700

    Add inc_dirs and lib_dirs functions to allow other distributions to locate Judy.h and libJudy.so

--- lib/Alien/Judy.pm
+++ lib/Alien/Judy.pm
@@ -1,5 +1,47 @@
 package Alien::Judy;
 
-$VERSION = '0.08';
+use strict;
+use warnings;
+
+use vars qw( @EXPORT_OK %EXPORT_TAGS $VERSION );
+
+use Config ();
+use File::Spec ();
+
+use Exporter ();
+*import = \&Exporter::import;
+
+@EXPORT_OK = qw( inc_dirs lib_dirs );
+%EXPORT_TAGS = (
+    all => \ @EXPORT_OK,
+);
+
+sub unique {
+    my %seen;
+    return
+        grep { ! $seen{$_}++ }
+        @_;
+}
+
+sub inc_dirs {
+    return
+        grep { $_ && -d }
+        unique(
+            Cwd::getcwd(),
+            map { File::Spec->catdir( $_, 'include' ) }
+            @Config::Config{qw( siteprefixexp prefixexp )}
+        );
+}
+
+sub lib_dirs {
+    return
+        grep { $_ && -d }
+        unique(
+            map { File::Spec->catdir( $_, 'Alien', 'Judy' ) }
+            @Config::Config{qw(sitearchexp sitearch)}
+        );
+}
+
+$VERSION = '0.09';
 
 1;
