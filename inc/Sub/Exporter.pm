31794b4d750c7a727f2e9492f76a9dbb1bb7e5aa
Josh ben Jore <jjore@cpan.org>
2010-06-29 23:56:59 -0700

    Repeat the stubbed Sub::Exporter variable to avoid warnings

--- inc/Sub/Exporter.pm
+++ inc/Sub/Exporter.pm
@@ -2,6 +2,6 @@ package main;
 
 # This is a stub package of Sub::Exporter. See Build.PL for more
 # notes.
-$main::INC_SUB_EXPORTER = 1;
+$main::INC_SUB_EXPORTER = $main::INC_SUB_EXPORTER = 1;
 
 1;
